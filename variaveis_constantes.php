8c4fa28c958d6305fd4bfbe7f4f0b80e7109aed3
Caio Rafael <ccaiocr@gmail.com>
2021-09-13 13:28:32 -0300

    Dia 13/09/2021

---
+++ variaveis_constantes.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VARIÁVEIS CONSTANTES</title>
+</head>
+<body>
+    <?php
+
+        define('BD_URL', 'endereço_bd_dev');
+       
+        
+    ?>
+</body>
+</html>
