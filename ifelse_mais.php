6de3058573e8df31897f26c4875797a2e60536ad
Caio Rafael <ccaiocr@gmail.com>
2021-09-20 14:12:22 -0300

    Dia 20/09/2021

---
+++ ifelse_mais.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IF E ELSE</title>
+    <link href="css/if.css" rel="stylesheet">
+</head>
+<body>
+
+    <?php
+        
+        $user_get_card = true;
+        $purchase_value = 300;
+
+        $fare_value = 50;
+        $take_discount = false;
+
+        if($user_get_card == true && $purchase_value >= 100) {
+            $fare_value = 0;
+            $take_discount = true;
+        } 
+    ?>
+
+    <h1>DETALHES DO PEDIDO</h1>
+
+    <p>Possui cartão da loja?
+        <?php
+            if($user_get_card == true) {
+                echo "SIM";
+            } else {
+                echo "NÃO";
+            }
+        ?>
+    </p>
+    
+    <p>Valor da compra: <?= $purchase_value?></p>        
+
+    <p>Recebeu desconto no frete?
+        <?php
+            if($take_discount == true) {
+                echo "SIM";
+            } else {
+                echo "NÃO";
+            }
+        ?>
+    </p>
+<p>Valor do Frete: <?= $fare_value?></p>    
+</body>
+</html>
---
+++ ifelse_operadores_logicos.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IF E ELSE OPERADORES LÓGICOS</title>
+</head>
+<body>
+    <?php
+        // == :IGUAL A     
+            // === :IDENTICO 
+            // != ou <> :DIFERENTE DE
+            // !== : NÃO IDENTICO
+            // > 
+            // >=
+            // <
+            // <=
+        
+            
+
+    ////////////////////////////////////////////////////////
+    if(8 >= 4 && 1 <= 9) {
+        echo "Verdadeiro";
+    } else {
+        echo "Falso";
+    }
+
+
+    if(5 == 3 XOR 10 == 5) {
+        echo "Verdadeiro";
+    } else {
+        echo "Falso";
+    }
+
+    
+    ?>
+    
+</body>
+</html>
